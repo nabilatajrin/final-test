946fcdd2340730b4c3fcb07cec78a5f5ead733fd
Nabila <nabilatajrin@gmail.com>
2018-10-09 19:03:26 +0600

    07:03PM 09.OCT.2018 email codes addes, app is okay, not sending email yet

--- app/src/main/java/com/example/rain/deviceadmin/MainActivity.java
+++ app/src/main/java/com/example/rain/deviceadmin/MainActivity.java
@@ -4,13 +4,71 @@ import android.app.Activity;
 import android.app.admin.DevicePolicyManager;
 import android.content.ComponentName;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.rain.deviceadmin.util.GmailSender;
+
 public class MainActivity extends Activity {
+
+    EditText myEmail, pass, sendToEmail, subject, text;
+
+    Button sendEmailButton;
+
+    String myEmailString, passString, sendToEmailString, subjectString, textString;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /*=========================================  send mail ===================================================*/
+        myEmail = (EditText) findViewById(R.id.emaileditText);
+        myEmail.setText("birds.phone.finder@gmail.com");
+        pass = (EditText) findViewById(R.id.passEditText);
+        pass.setText("123456abc@");
+        sendToEmail = (EditText) findViewById(R.id.sendToEmaileditText);
+        sendToEmail.setText("nabilatajrin@gmail.com");
+        subject = (EditText) findViewById(R.id.subjectEditText);
+        subject.setText("new test: call from another app");
+        text = (EditText) findViewById(R.id.textEditText);
+        text.setText("body4");
+        sendEmailButton = (Button) findViewById(R.id.button);
+
+        final SendEmailTask sendEmailTask = new SendEmailTask();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                sendEmailButton.performClick();
+            }
+        }, 100);
+
+
+        sendEmailButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myEmailString = myEmail.getText().toString();
+                passString = pass.getText().toString();
+                sendToEmailString = sendToEmail.getText().toString();
+                subjectString = subject.getText().toString();
+                textString = text.getText().toString();
+
+                sendEmailTask.execute();
+            }
+        });
+
+
+
+
+        /*=========================================  end of send mail ===================================================*/
+
 
         ComponentName cn=new ComponentName(this, AdminReceiver.class);
         DevicePolicyManager mgr=
@@ -39,4 +97,41 @@ public class MainActivity extends Activity {
 
         finish();
     }
+
+
+    /*=========================================== class to send email =================================================*/
+
+    class SendEmailTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            Log.i("Email sending", "sending start");
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                GmailSender sender = new GmailSender(myEmailString, passString);
+                //subject, body, sender, to
+                sender.sendMail(subjectString,
+                        textString,
+                        myEmailString,
+                        sendToEmailString);
+
+                Log.i("Email sending", "send");
+            } catch (Exception e) {
+                Log.i("Email sending", "cannot send");
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+        }
+    }
+
+    /*=========================================== end of class to send email =================================================*/
 }
