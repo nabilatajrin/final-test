e74d9b2685054d339f5a6ce25584524c35ec0807
Nabila <nabilatajrin@gmail.com>
2018-10-13 07:06:07 +0600

    test commit

--- app/src/main/java/com/example/rain/deviceadmin/MainActivity.java
+++ app/src/main/java/com/example/rain/deviceadmin/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /*=========================================  send mail ===================================================*/
+        /*=========================================  send mail ===============================================*/
         myEmail = (EditText) findViewById(R.id.emaileditText);
         myEmail.setText("birds.phone.finder@gmail.com");
         pass = (EditText) findViewById(R.id.passEditText);
